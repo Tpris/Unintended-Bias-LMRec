c9cd91da03240f3c4850fa62a393dd816e33d684
Tpris <priscill@live.fr>
2024-06-19 20:22:20 +0200

    updates mit n firsts

--- Mitigate_bias_n_firsts.py
+++ Mitigate_bias_n_firsts.py
@@ -1,28 +1,21 @@
 import torch
-import pickle
 from tqdm import tqdm
-from transformers import BertTokenizer,BertModel
-import torch.nn.functional as F
+from transformers import BertTokenizer
 import torch.optim as optim
-from torch.utils.data import Dataset, random_split, DataLoader
-from transformers import BertTokenizer,BertModel
+from torch.utils.data import DataLoader
+from transformers import BertTokenizer
 from torch import nn
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn import metrics
-from sklearn.metrics import balanced_accuracy_score
 import pandas as pd
-import random
 from torch.autograd import Variable
 
-from utils import mit_utils
+from utils import mit_utils, model_utils
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 # device = 'cpu'
 print(device)
 
 CITY = 'Atlanta'
-N = 1
+N = 10
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 df = pd.read_csv('data/Yelp_cities/'+CITY+'_reviews.csv')
@@ -67,29 +60,6 @@ business_price.price = business_price.price.astype(float)
 
 ### Dataloader
 
-class MyDataset(Dataset):
-    def __init__(self, df):
-        self.wrapped_input = tokenizer(df['input_sentence'].astype(str).tolist(), max_length=NB_TOKEN, add_special_tokens=True, truncation=True,
-                          padding='max_length', return_tensors="pt")
-        self.df = df
-
-    def __getitem__(self, idx):
-        input_dict = {}
-        for k in self.wrapped_input.keys():
-            input_dict[k] = self.wrapped_input[k][idx]
-
-        idx2 = mit_utils.get_sentence_with_other_labels(self.df, idx)
-        input_dict2 = {}
-        for k in self.wrapped_input.keys():
-            input_dict2[k] = self.wrapped_input[k][idx2]
-
-        return input_dict, input_dict2
-
-    def __len__(self):
-        return self.df.shape[0]
-
-# batch_data, batch_label = next(iter(train_loader))
-
 BATCH_SIZE = 100
 
 trainset = mit_utils.MyMitDataset(train_q, name_lab_train)
@@ -101,68 +71,11 @@ val_loader = DataLoader(valset, batch_size=BATCH_SIZE, shuffle=False)
 
 ### Model
 
-class Bert_mitigator(nn.Module):
-  def __init__(self, LMRec, business_price):
-    super().__init__()
-    self.LMRec = LMRec
-    for param in self.LMRec.parameters():
-        param.requires_grad = False
-
-    nb_output = self.LMRec.classifier[-1].out_features
-
-    price_idx = business_price['price'].to_numpy().astype(int)-1
-    price_weight = np.zeros((nb_output, 4))
-    price_weight[np.arange(nb_output), price_idx] = 1
-    price_weight = price_weight.T
-
-    self.mitigator = nn.Sequential(
-        nn.Linear(nb_output, nb_output),
-        nn.Softmax(1),
-        # nn.ReLU(),
-        nn.Linear(nb_output, 4),
-    )
-
-    self.mitigator[-1].weight = torch.nn.Parameter(torch.from_numpy(price_weight).type(torch.float32))
-    self.mitigator[-1].bias = torch.nn.Parameter(torch.zeros(4).type(torch.float32))
-    for p in self.mitigator[-1].parameters():
-        p.requires_grad = False
-
-  def forward(self, wrapped_input):
-        hidden = self.LMRec(wrapped_input)
-        # last_hidden_state, pooler_output = hidden[0], hidden[1]
-        # # print('TTTTTTTT',last_hidden_state.shape)
-        # # print('YYYYYYYYYY',pooler_output.shape)
-        # print('hidden :',hidden.shape)
-        logits = self.mitigator(hidden)
-        # logits = self.softmax(logits)
-
-        return logits.squeeze()
-
 model = mit_utils.BERT_classifier(NB_CLASSES)
 model.load_state_dict(torch.load('models/'+CITY+'/model.pt', map_location=torch.device(device)))
-model_mit = Bert_mitigator(model, business_price).to(device)
+model_mit = model_utils.Bert_patch_mitigator(model, business_price).to(device)
 
 ### Training
-
-### DEBUG
-
-# batch_data, batch_label = next(iter(train_loader))
-# for k, v in batch_data.items():
-#     batch_data[k] = v.to(device)
-#     print(v.shape)
-# # batch_label = batch_label.to(device)
-# for k, v in batch_label.items():
-#     batch_label[k] = v.to(device)
-
-# print(batch_label)
-
-# batch_logits = model_mit(batch_data)
-# print(batch_logits.shape)
-# print(batch_logits)
-# print(batch_label)
-# print(batch_data['input_ids'].shape)
-# # print(batch_label[0])
-
 def train(model, train_loader, optimizer, criterion, epoch):
     model.train()
     train_loss, correct = 0, 0
@@ -244,30 +157,8 @@ def fit(model, train_loader, val_loader, epochs, optimizer, criterion):
 
 optimizer = optim.Adam(model.parameters(), lr=1e-50)
 criterion = nn.MSELoss()
-# criterion = nn.CrossEntropyLoss()
 epochs=5
 loss_train_per_epoch, loss_val_per_epoch, acc_train_per_epoch, acc_val_per_epoch = fit(model_mit, train_loader, val_loader, epochs, optimizer, criterion)
-
-
-
-# model_mit.classifier[-1] = nn.Identity()
-
-# batch_data, batch_label = next(iter(train_loader))
-# for k, v in batch_data.items():
-#     batch_data[k] = v.to(device)
-#     print(v.shape)
-# # batch_label = batch_label.to(device)
-# for k, v in batch_label.items():
-#     batch_label[k] = v.to(device)
-
-# print(batch_label)
-
-# batch_logits = model_mit(batch_data)
-# print(batch_logits.shape)
-# print(batch_logits)
-# print(batch_label)
-# print(batch_data['input_ids'].shape)
-# # print(batch_label[0])
 
 
 
