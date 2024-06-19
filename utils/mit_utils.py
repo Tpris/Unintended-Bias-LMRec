121a10907001efed00aea59504da2fe1e277195e
Tpris <priscill@live.fr>
2024-06-19 18:41:56 +0200

    Update utils files

--- utils/mit_utils.py
+++ utils/mit_utils.py
@@ -1,13 +1,9 @@
 import numpy as np
 import random
 from tqdm import tqdm
-from transformers import BertTokenizer,BertModel
-import torch.nn.functional as F
-from torch.utils.data import Dataset, random_split, DataLoader
-from transformers import BertTokenizer,BertModel
-from torch import nn
 from torch.autograd import Variable
 import torch
+from torch import nn
 
 def get_sup(df, nb=100):
     df_count = df.groupby('business_id').count()
@@ -78,6 +74,11 @@ def get_name_labels(input_questions):
     name_lab = input_questions[['example_label', 'label']]
     name_lab = name_lab.groupby('example_label')['label'].unique()
     return name_lab
+
+def load_weight_mitigator(model, model_dir):
+    model.load_state_dict(torch.load(model_dir))
+    model.mitigator[-1] = nn.Identity()
+    return model
 
 def train_review(model, train_loader, optimizer, criterion, epoch, device):
     model.train()
@@ -220,104 +221,3 @@ def fit(model1, model2, train_loader1, val_loader1, train_loader2, val_loader2, 
         acc_val_mit_per_epoch += [val_mit_acc]
 
     return loss_train_review_per_epoch, loss_val_review_per_epoch, acc_train_review_per_epoch, acc_val_review_per_epoch, loss_train_mit_per_epoch, loss_val_mit_per_epoch, acc_train_mit_per_epoch, acc_val_mit_per_epoch
-
-
-class BERT_classifier(nn.Module):
-    def __init__(self, num_label):
-        super().__init__()
-        self.bert = BertModel.from_pretrained("bert-base-uncased")
-        for param in self.bert.parameters():
-            param.requires_grad = False
-        self.classifier = nn.Sequential(
-            nn.Linear(self.bert.config.hidden_size, 1024),
-            nn.ReLU(),
-            nn.Linear(1024, num_label),
-        )
-
-    def forward(self, wrapped_input):
-        hidden = self.bert(**wrapped_input)
-        last_hidden_state, pooler_output = hidden[0], hidden[1]
-        logits = self.classifier(pooler_output)
-        # logits = self.softmax(logits)
-
-        return logits.squeeze()
-
-class Bert_mitigator(nn.Module):
-  def __init__(self, LMRec, business_price, DECODER, EMBEDDING):
-    super().__init__()
-    self.LMRec = LMRec
-
-    if DECODER or EMBEDDING:
-        for param in self.LMRec.parameters():
-            param.requires_grad = False
-        if DECODER:
-            for param in self.LMRec.classifier[-1].parameters():
-                param.requires_grad = True
-        if EMBEDDING:
-            for param in self.LMRec.bert.embeddings.parameters():
-                param.requires_grad = True
-
-    nb_output = self.LMRec.classifier[-1].out_features
-
-    price_idx = business_price['price'].to_numpy().astype(int)-1
-    price_weight = np.zeros((nb_output, 4))
-    price_weight[np.arange(nb_output), price_idx] = 1
-    price_weight = price_weight.T
-
-    self.mitigator = nn.Sequential(
-        # nn.Softmax(1),
-        nn.Linear(nb_output, 4),
-    )
-
-    self.mitigator[-1].weight = torch.nn.Parameter(torch.from_numpy(price_weight).type(torch.float32))
-    self.mitigator[-1].bias = torch.nn.Parameter(torch.zeros(4).type(torch.float32))
-    for p in self.mitigator[-1].parameters():
-        p.requires_grad = False
-
-  def forward(self, wrapped_input):
-        hidden = self.LMRec(wrapped_input)
-        logits = self.mitigator(hidden)
-        return logits.squeeze()
-  
-
-class MyMitDataset(Dataset):
-    def __init__(self, input_data, name_lab):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.wrapped_input = tokenizer(input_data['input_sentence'].astype(str).tolist(), max_length=512, add_special_tokens=True, truncation=True,
-                          padding='max_length', return_tensors="pt")
-        self.input_data = input_data
-        self.name_lab= name_lab
-
-    def __getitem__(self, idx):
-        input_dict = {}
-        for k in self.wrapped_input.keys():
-            input_dict[k] = self.wrapped_input[k][idx]
-
-        idx2 = get_sentence_with_other_labels(self.input_data, idx, self.name_lab)
-        input_dict2 = {}
-        for k in self.wrapped_input.keys():
-            input_dict2[k] = self.wrapped_input[k][idx2]
-
-        return input_dict, input_dict2
-
-    def __len__(self):
-        return self.input_data.shape[0]
-    
-
-class MyDataset(Dataset):
-    def __init__(self, sentence, labels, LABELS, NB_CLASSES):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.wrapped_input = tokenizer(sentence, max_length=512, add_special_tokens=True, truncation=True,
-                          padding='max_length', return_tensors="pt")
-        lab_idx = np.array([LABELS.index(l) for l in labels])
-        self.labels = np.zeros((lab_idx.size, NB_CLASSES))
-        self.labels[np.arange(lab_idx.size), lab_idx] = 1
-        # self.labels = np.array(labels).astype(float)
-    def __getitem__(self, idx):
-        input_dict = {}
-        for k in self.wrapped_input.keys():
-            input_dict[k] = self.wrapped_input[k][idx]
-        return input_dict, self.labels[idx]
-    
-    def __len__(self):
-        return len(self.labels)
--- utils/model_utils.py
+++ utils/model_utils.py
@@ -1,41 +1,133 @@
-def create_model(max_len, labels, learning_rate=5e-5, experiment='5f', subexp='freezeBoth'):
-    encoder = TFBertModel.from_pretrained("bert-base-uncased")
-    print(experiment)
+from transformers import BertTokenizer,BertModel
+import torch.nn.functional as F
+from torch.utils.data import Dataset, random_split, DataLoader
+from transformers import BertTokenizer,BertModel
+from torch import nn
+import torch
+import numpy as np
 
-    if experiment == '5n':
-        print('Freezing BERT weights')
+from utils.mit_utils import get_sentence_with_other_labels
 
-        if subexp == 'freezeEmbeddings':
-            print('freezing embeddings')
-            encoder.bert.embeddings.trainable = False
-        elif subexp == 'freezeEncoders':
-            print('freezing encoder layers')
-            for layer in encoder.bert.encoder.layer:
-                layer.trainable = False
-        elif subexp == 'freezeBoth':
-            print('freezing embeddings')
-            encoder.bert.embeddings.trainable = False
-            print('freezing encoder layers')
-            for layer in encoder.bert.encoder.layer:
-                layer.trainable = False
-        else:
-            assert False, 'Unrecognized sub experiment type'
+class BERT_classifier(nn.Module):
+    def __init__(self, num_label):
+        super().__init__()
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+        for param in self.bert.parameters():
+            param.requires_grad = False
+        self.classifier = nn.Sequential(
+            nn.Linear(self.bert.config.hidden_size, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, num_label),
+        )
 
-    input_ids = layers.Input(shape=(max_len,), dtype=tf.int32)
-    attention_mask = layers.Input(shape=(max_len,), dtype=tf.int32)
+    def forward(self, wrapped_input):
+        hidden = self.bert(**wrapped_input)
+        last_hidden_state, pooler_output = hidden[0], hidden[1]
+        logits = self.classifier(pooler_output)
 
-    embedding = encoder(
-        input_ids, attention_mask=attention_mask
-    )['pooler_output']
+        return logits.squeeze()
+    
 
-    dense = layers.Dense(1024, activation='relu')(embedding)
-    out = layers.Dense(len(labels), activation='softmax')(dense)
+class Bert_patch_mitigator(nn.Module):
+  def __init__(self, LMRec):
+    super().__init__()
+    self.LMRec = LMRec
+    for param in self.LMRec.parameters():
+        param.requires_grad = False
 
-    model = keras.Model(
-        inputs=[input_ids, attention_mask],
-        outputs=out, )
+    nb_output = self.LMRec.classifier[-1].out_features
 
-    loss = keras.losses.SparseCategoricalCrossentropy()
-    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
-    model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
-    return model
+    self.mitigator = nn.Sequential(
+        nn.Linear(nb_output, nb_output),
+        nn.Softmax(1),
+        nn.Linear(nb_output, 4),
+    )
+
+  def forward(self, wrapped_input):
+        hidden = self.LMRec(wrapped_input)
+        logits = self.mitigator(hidden)
+
+        return logits.squeeze()
+
+class Bert_mitigator(nn.Module):
+  def __init__(self, LMRec, business_price, DECODER, EMBEDDING):
+    super().__init__()
+    self.LMRec = LMRec
+
+    if DECODER or EMBEDDING:
+        for param in self.LMRec.parameters():
+            param.requires_grad = False
+        if DECODER:
+            for param in self.LMRec.classifier[-1].parameters():
+                param.requires_grad = True
+        if EMBEDDING:
+            for param in self.LMRec.bert.embeddings.parameters():
+                param.requires_grad = True
+
+    nb_output = self.LMRec.classifier[-1].out_features
+
+    price_idx = business_price['price'].to_numpy().astype(int)-1
+    price_weight = np.zeros((nb_output, 4))
+    price_weight[np.arange(nb_output), price_idx] = 1
+    price_weight = price_weight.T
+
+    self.mitigator = nn.Sequential(
+        nn.Softmax(1),
+        nn.Linear(nb_output, 4),
+    )
+
+    self.mitigator[-1].weight = torch.nn.Parameter(torch.from_numpy(price_weight).type(torch.float32))
+    self.mitigator[-1].bias = torch.nn.Parameter(torch.zeros(4).type(torch.float32))
+    for p in self.mitigator[-1].parameters():
+        p.requires_grad = False
+
+  def forward(self, wrapped_input):
+        hidden = self.LMRec(wrapped_input)
+        logits = self.mitigator(hidden)
+        return logits.squeeze()
+    
+
+
+  
+
+class MyMitDataset(Dataset):
+    def __init__(self, input_data, name_lab):
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.wrapped_input = tokenizer(input_data['input_sentence'].astype(str).tolist(), max_length=512, add_special_tokens=True, truncation=True,
+                          padding='max_length', return_tensors="pt")
+        self.input_data = input_data
+        self.name_lab= name_lab
+
+    def __getitem__(self, idx):
+        input_dict = {}
+        for k in self.wrapped_input.keys():
+            input_dict[k] = self.wrapped_input[k][idx]
+
+        idx2 = get_sentence_with_other_labels(self.input_data, idx, self.name_lab)
+        input_dict2 = {}
+        for k in self.wrapped_input.keys():
+            input_dict2[k] = self.wrapped_input[k][idx2]
+
+        return input_dict, input_dict2
+
+    def __len__(self):
+        return self.input_data.shape[0]
+    
+
+class MyDataset(Dataset):
+    def __init__(self, sentence, labels, LABELS, NB_CLASSES):
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.wrapped_input = tokenizer(sentence, max_length=512, add_special_tokens=True, truncation=True,
+                          padding='max_length', return_tensors="pt")
+        lab_idx = np.array([LABELS.index(l) for l in labels])
+        self.labels = np.zeros((lab_idx.size, NB_CLASSES))
+        self.labels[np.arange(lab_idx.size), lab_idx] = 1
+        # self.labels = np.array(labels).astype(float)
+    def __getitem__(self, idx):
+        input_dict = {}
+        for k in self.wrapped_input.keys():
+            input_dict[k] = self.wrapped_input[k][idx]
+        return input_dict, self.labels[idx]
+    
+    def __len__(self):
+        return len(self.labels)
