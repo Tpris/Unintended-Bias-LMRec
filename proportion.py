9322100ce99360256c0129b4a2c4314ed1a370cd
Tpris <priscill@live.fr>
2024-06-23 15:31:00 +0200

    Rename function

--- proportion.py
+++ proportion.py
@@ -6,7 +6,7 @@ path = "bias_analysis/yelp/figures/"
 df_eth = pd.read_csv(path+"Atlanta_raceB_price_lvl.csv")[['label', 'price_lvl', 'counts']]
 df_gender = pd.read_csv(path+"Atlanta_genderB_price_lvl.csv")[['label', 'price_lvl', 'counts']]
 
-def get_y(df,label):
+def get_values_per_price_range(df,label):
   y = df.loc[df['label'] == label][['counts', 'price_lvl']]
   y.index = y.price_lvl
   y = y.counts
@@ -20,8 +20,8 @@ def plot_counts(df,label1,label2,title, name_saved):
     fig, ax = plt.subplots(figsize=(12, 8))
     x = np.arange(len(df.price_lvl.unique()))
     bar_width = 0.4
-    y1 = get_y(df, label1)
-    y2 = get_y(df, label2)
+    y1 = get_values_per_price_range(df, label1)
+    y2 = get_values_per_price_range(df, label2)
     b1 = ax.bar(x, y1, width=bar_width, label=label1)
     b2 = ax.bar(x + bar_width, y2, width=bar_width, label=label2)
     
