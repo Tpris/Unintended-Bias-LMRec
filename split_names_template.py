578f599bd07f47400de32351bd25b1a0044d9bea
Tpris <priscill@live.fr>
2024-06-23 15:44:59 +0200

    :memo: Add documentation on split questions file

--- split_names_template.py
+++ split_names_template.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+# Set to true if you don't want to split templates
 ONLY_NAMES = False
 
 def check_templates(row, sentences_part):
@@ -12,6 +13,7 @@ def get_selected_sentences(input_questions, list_tpl):
     msk = input_questions.apply(lambda r: check_templates(r,list_tpl), axis=1)
     return input_questions[msk]
 
+# Load names and rearrange their labels
 Names = pd.read_json('data/bias_analysis/example_labels/names.json')
 Names = Names.transpose()
 Names['label'] = Names.index
@@ -27,7 +29,9 @@ lst = []
 tot_train = []
 tot_validation = []
 tot_test = []
+# Split data for each group
 for r in names_grp['names']:
+    # 3/5 : Train, 1/5 : validation, 1/5 : test
     train, test = train_test_split(r, test_size=0.2, random_state=42)
     train, validation = train_test_split(train, test_size=0.25, random_state=42)
     lst += [[train, validation, test]]
@@ -35,9 +39,11 @@ for r in names_grp['names']:
     tot_validation  += validation
     tot_test        += test
 
+# Convert to dataframe format and save to csv file
 df = pd.DataFrame(lst, columns = ['train', 'validation', 'test'], index = names_grp['labels']) 
 df.to_csv('data/bias_analysis/yelp/input_sentences/names_ratio.csv')
 
+# Split questions with names
 input_questions = pd.read_csv('data/bias_analysis/yelp/input_sentences/save/names.csv')
 
 train      = input_questions[input_questions.example_label.isin(tot_train)].reset_index(drop=True)
@@ -55,9 +61,12 @@ if ONLY_NAMES:
 Templates = pd.read_json('data/bias_analysis/templates/yelp_templates.json')
 Templates = Templates['names'].to_list()
 
+# Split templates
+# 3/5 : Train, 1/5 : validation, 1/5 : test
 train_tpl, test_tpl = train_test_split(Templates, test_size=0.2, random_state=42)
 train_tpl, validation_tpl = train_test_split(train_tpl, test_size=0.25, random_state=42)
 
+# Convert to dataframe format
 train_tpl_df = pd.DataFrame(train_tpl, columns = ['template']) 
 train_tpl_df['part'] = 'Train'
 
@@ -68,6 +77,7 @@ test_tpl_df = pd.DataFrame(test_tpl, columns = ['template'])
 test_tpl_df['part'] = 'Test'
 
 templates_save = pd.concat([train_tpl_df,val_tpl_df,test_tpl_df])
+# Save template lists
 templates_save.to_csv('data/bias_analysis/yelp/input_sentences/template_split.csv')
 print(templates_save)
 
@@ -75,6 +85,7 @@ train_sentences = get_selected_sentences(train, train_tpl)
 val_sentences   = get_selected_sentences(validation, validation_tpl)
 test_sentences  = get_selected_sentences(test, test_tpl)
 
+# Save questions based on template split
 train_sentences.to_csv('data/bias_analysis/yelp/input_sentences/names_templates_train.csv')
 val_sentences.to_csv('data/bias_analysis/yelp/input_sentences/names_templates_validation.csv')
 test_sentences.to_csv('data/bias_analysis/yelp/input_sentences/names_templates_test.csv')
